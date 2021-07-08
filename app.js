5b4b8f00f88aaa1e48e72fd14118042c3f39b49c
Josh <joshetcarson@gmail.com>
2021-07-08 17:48:31 -0400

    Added index.html, styles.css, app.js basic files and image file for figma wireframe.

---
+++ app.js
@@ -0,0 +1,42 @@
+// MVP:
+//
+// Query user input to determine which word to search
+// Error handling for missing words
+// Fetch dictionary and thesaurus APIs from Merriam-Webster website
+// Access definition and synonyms from JSON object
+// Populate the results section with definition and synonyms
+//
+// Stretch Goals:
+//
+// Add an image of word searched from appropriate API
+// Create small scrolling gallery of available images
+// Offer audible definition when available
+
+const app = {};
+
+app.desiredWord = "house";
+app.dictKey = "a9cac565-bc55-4024-9e9a-7f276804fe69";
+app.dictURL = new URL(
+	`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${app.desiredWord}?key=${app.dictKey}`
+);
+
+app.thesaurusKey = "fb667759-bffa-458e-99cc-52ad3a241442";
+app.thesaurusURL = new URL(
+	`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${app.desiredWord}?key=${app.thesaurusKey}`
+);
+
+fetch(app.dictURL)
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (jsonResult) {
+		console.log(jsonResult);
+	});
+
+fetch(app.thesaurusURL)
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (jsonResult) {
+		console.log(jsonResult);
+	});
