314de390cdc33487481221899a08f63e9e5c2060
Josh <joshetcarson@gmail.com>
2021-07-10 11:45:31 -0400

    Added user input function to get desiredWord, updated displayResult function to show definitions and synonyms in the resultsList UL

--- app.js
+++ app.js
@@ -12,130 +12,98 @@
 // Create small scrolling gallery of available images
 // Offer audible definition when available
 
-// const app = {};
-
-// app.desiredWord = "house";
-// app.dictKey = "a9cac565-bc55-4024-9e9a-7f276804fe69";
-// app.dictURL = new URL(
-//   `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${app.desiredWord}?key=${app.dictKey}`
-// );
-
-// app.thesaurusKey = "fb667759-bffa-458e-99cc-52ad3a241442";
-// app.thesaurusURL = new URL(
-//   `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${app.desiredWord}?key=${app.thesaurusKey}`
-// );
-
-// fetch(app.dictURL)
-//   .then(function (res) {
-//     return res.json();
-//   })
-//   .then(function (jsonResult) {
-//     console.log(jsonResult);
-//   });
-
-// fetch(app.thesaurusURL)
-//   .then(function (res) {
-//     return res.json();
-//   })
-//   .then(function (jsonResult) {
-//     console.log(jsonResult);
-//   });
-
 const app = {};
 
-app.desiredWord = "child";
-
-// app.dictKey = "a9cac565-bc55-4024-9e9a-7f276804fe69";
-// app.dictURL = new URL(
-// 	`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${app.desiredWord}?key=${app.dictKey}`
-// );
-
-app.thesaurusKey = "fb667759-bffa-458e-99cc-52ad3a241442";
-app.thesaurusURL = new URL(
-	`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${app.desiredWord}?key=${app.thesaurusKey}`
-);
-
-// app.dictionaryAPI = function () {
-// 	fetch(app.dictURL)
-// 		.then(function (res) {
-// 			return res.json();
-// 		})
-// 		.then(function (jsonResult) {
-// 			console.log(jsonResult);
-// 		});
-// };
-
-app.thesaurusAPI = function () {
-	fetch(app.thesaurusURL)
-		.then(function (res) {
-			return res.json();
-		})
-		.then(function (jsonResult) {
-			app.definitions = jsonResult[0].shortdef;
-			console.log(app.definitions);
-			app.synonyms = jsonResult[0].meta.syns;
-			console.log(app.synonyms);
-		});
-};
-
-app.init = function () {
-	app.desiredWord;
-	// app.dictionaryAPI();
-	app.thesaurusAPI();
-};
-
-app.init();
-
-app.desiredWord = document.querySelector("input[type=text").value;
 // getting the results from the API Call
 
+app.getDesiredWord = function () {
+	// Selecting the form element
+	app.form = document.querySelector("form");
+
+	// Adding the event listener to the submit button
+	app.form.addEventListener("submit", function (event) {
+		event.preventDefault();
+		app.desiredWord = document.getElementById("search").value;
+		console.log(app.desiredWord);
+		app.getResult();
+	});
+};
+
 app.getResult = function () {
-	app.desiredWord = "house";
+	app.dictKey = "a9cac565-bc55-4024-9e9a-7f276804fe69";
+	app.dictURL = new URL(
+		`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${app.desiredWord}?key=${app.dictKey}`
+	);
 
 	app.thesaurusKey = "fb667759-bffa-458e-99cc-52ad3a241442";
 	app.thesaurusURL = new URL(
 		`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${app.desiredWord}?key=${app.thesaurusKey}`
 	);
 
-	fetch(app.thesaurusURL)
+	fetch(app.dictURL)
 		.then(function (res) {
 			return res.json();
 		})
 		.then(function (jsonResult) {
-			app.definitions = jsonResult[0].shortdef;
-			//   console.log(app.definitions);
-			app.synonyms = jsonResult[0].meta.syns;
-			console.log(app.synonyms);
-
+			app.definitions = jsonResult;
+			console.log(app.definitions);
+			// console.log(app.definitions);
 			// calling the display function
-			app.displayResult(app.definitions);
+			// app.displayResult(app.definitions);
+
+			// Error handling if word cannot be found (meta == undefined)
+			// if (jsonResult.meta === undefined) {
+			// 	alert("Unable to find that word. Check your spelling!");
+			// }
+		})
+		.then(function () {
+			fetch(app.thesaurusURL)
+				.then(function (res) {
+					return res.json();
+				})
+				.then(function (jsonResult) {
+					// console.log(jsonResult);
+					// app.definitions = jsonResult[0].shortdef;
+					// console.log(app.definitions);
+					app.synonyms = jsonResult[0].meta.syns[0];
+					console.log(app.synonyms);
+					app.displayResult(app.definitions, app.synonyms);
+				});
 		});
 };
 
 // Displaying the resullts on the page
 
-app.displayResult = function (definations) {
-	// Selecting the form
-	app.form = document.querySelector("form");
-	// Adding the event listenet to the submit button
-	app.form.addEventListener("submit", function (event) {
-		// preventing the form to get refresh
-		event.preventDefault();
-		// app.desiredWord = document.querySelector("input[type=text").value;
+app.displayResult = function (definitionArray, synonymsArray) {
+	// Creating an array with the definitions of the word to display to screen
+	const resultsList = document.querySelector(".resultsList");
+	resultsList.innerHTML = "";
 
-		console.log("Button clicked");
-		// creating the filing the defination
+	// definitionsArray.forEach(function (definitionObj) {
+	// 	// console.log(definitionArray);
+	// 	const liEl = document.createElement("li");
+	// 	console.log(liEl);
+	// 	liEl.appendChild(definitionObj);
+	// 	definitionsList.appendChild(liEl);
+	// });
 
-		definations.forEach(function (definationArray) {
-			console.log(definationArray);
-		});
-	});
+	for (let i = 0; i < definitionArray.length; i++) {
+		let liEl = document.createElement("li");
+		liEl.innerHTML = `${definitionArray[i].shortdef}`;
+		resultsList.appendChild(liEl);
+	}
+
+	for (let i = 0; i < synonymsArray.length; i++) {
+		let liEl = document.createElement("li");
+		liEl.innerHTML = `${synonymsArray[i]}`;
+		resultsList.appendChild(liEl);
+	}
 };
 
 // Kicking off
 app.init = function () {
 	// calling the get result
-	app.getResult();
+	app.getDesiredWord();
 };
 
 // Calling init function
